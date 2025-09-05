420506ee5ded1d2d5853f568d619f10c1854ec46
Dontsov Roman <dontsovroman@gmail.com>
2025-09-05 17:58:21 +0300

    Replace issue to shared component

--- src/frontend/components/issue/Issue.tsx
+++ src/frontend/components/issue/Issue.tsx
@@ -1,9 +1,10 @@
 import React, { Suspense, useMemo } from "react";
-import { Text, Badge, Inline, Box, Spinner, Stack, User, Image } from "@forge/react";
+import { Box, Spinner } from "@forge/react";
 import { BadgeProps } from "@forge/react/out/types";
 import { useQuery } from "@tanstack/react-query";
 import { getIssueOption } from "../../queries/options";
 import { getIssueKey } from "../../utils/getIssueKey";
+import { IssueItem } from "../shared/IssueItem";
 
 type Props = {
     title: string;
@@ -22,26 +23,21 @@ export const Issue: React.FC<Props> = ({ title }) => {
         return data?.fields?.statusCategory?.colorName ? colorMap[data?.fields.statusCategory.colorName] : 'default';
     }, [data]);
     const accountId = useMemo(() => data?.fields?.assignee?.accountId || '', [data]);
-    const showSummary = useMemo(() => data?.fields?.issuetype?.name && data?.fields?.summary, [data]);
 
     return (
         <Suspense fallback={<Spinner size="large" />}>
             <Box padding='space.200' backgroundColor="color.background.accent.yellow.subtler">
-                {isLoading && <Spinner size="large" />}
-                <Stack space="space.100">
-                    <Inline space="space.100">
-                        <Text>{data?.key}</Text>
-                        <Badge appearance={badgeAppearance}>{data?.fields?.status?.name || 'Ticket not found'}</Badge>
-                    </Inline>
-                    {showSummary && <Inline space="space.025" alignBlock="start">
-                        <Text><Image src={data?.fields?.issuetype.iconUrl || ''} size="xsmall" width={15} /></Text>
-                        <Text>{data?.fields?.summary}</Text>
-                    </Inline>}
-                    {accountId && <Inline alignInline="center" alignBlock="center">
-                        <Text>Assignee:</Text>
-                        <User accountId={accountId} />
-                    </Inline>}
-                </Stack>
+                {isLoading ?
+                    <Spinner size="large" /> :
+                    <IssueItem
+                        accountId={accountId}
+                        issueKey={data?.key}
+                        badgeAppearance={badgeAppearance}
+                        issueType={data?.fields?.status?.name}
+                        iconUrl={data?.fields?.issuetype.iconUrl}
+                        summary={data?.fields?.summary}
+                    />
+                }
             </Box>
         </Suspense>
     );
---
+++ src/frontend/components/shared/IssueItem.tsx
@@ -0,0 +1,39 @@
+import React, { useMemo } from "react";
+import { Text, Badge, Inline, Stack, User, Image } from "@forge/react";
+import { BadgeProps } from "@forge/react/out/types";
+
+type Props = {
+    issueKey?: string;
+    badgeAppearance?: BadgeProps['appearance'];
+    accountId?: string;
+    summary?: string;
+    iconUrl?: string;
+    issueType?: string;
+};
+
+export const IssueItem: React.FC<Props> = ({
+    issueKey = '',
+    accountId = '',
+    summary = '',
+    iconUrl = '',
+    badgeAppearance = 'default',
+    issueType = 'TIcket not found',
+}) => {
+    const showSummary = useMemo(() => Boolean(iconUrl && summary), [iconUrl, summary]);
+    return (
+        <Stack space="space.100">
+            <Inline space="space.100">
+                <Text>{issueKey}</Text>
+                <Badge appearance={badgeAppearance}>{issueType}</Badge>
+            </Inline>
+            {showSummary && <Inline space="space.025" alignBlock="start">
+                <Text><Image src={iconUrl || ''} size="xsmall" width={15} /></Text>
+                <Text>{summary}</Text>
+            </Inline>}
+            {accountId && <Inline alignInline="center" alignBlock="center">
+                <Text>Assignee:</Text>
+                <User accountId={accountId} />
+            </Inline>}
+        </Stack>
+    );
+};
