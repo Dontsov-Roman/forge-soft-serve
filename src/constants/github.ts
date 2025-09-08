355249607f74058ffefb036afd04630629c0fd65
Dontsov Roman <dontsovroman@gmail.com>
2025-09-08 18:50:08 +0300

    clean up

--- src/constants/github.ts
+++ src/constants/github.ts
@@ -1,4 +1,3 @@
-export const GIT_HUB_ACCESS_TOKEN = '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';
 export const GIT_HUB_ORG = 'roman-dontsov';
 export const GIT_HUB_VERSION = '2022-11-28';
 export const GIT_HUB_STORE_KEY = 'GIT_HUB_STORE_KEY';
